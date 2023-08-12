34856d5e3070f379b181ade162735cb5fc420d1f
smola14 <dominik.smolinsky@icloud.com>
2023-08-12 13:32:09 +0200

    install packages

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import BuLMA from 'bulma'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(BuLMA)
 
 app.mount('#app')
