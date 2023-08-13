63e2f37a35345e7e6bb88f9a0815be9c3f132dda
smola14 <dominik.smolinsky@icloud.com>
2023-08-13 11:29:59 +0200

    deleted tasks view

---
+++ src/views/DeletedTasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div >
+    <ul>
+      <button @click="$router.push('/')">&lt; back to active tasks</button>
+      <h1 class="title">Deleted tasks</h1>
+
+      <li v-for="(item, index) in todoStore.items" :key="index" class="mt-2">
+        <div v-if="item.deleted">
+          <span>
+            {{ item.name }}
+          </span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useStoreTodo } from '@/stores/storeTodo.js'
+export default {
+  computed: {
+    ...mapStores(useStoreTodo),
+  },
+}
+</script>
