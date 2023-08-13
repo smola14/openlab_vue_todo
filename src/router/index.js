f5d6c9af5ba12870319e2ca8232b7a896edb1590
smola14 <dominik.smolinsky@icloud.com>
2023-08-13 14:02:57 +0200

    rename component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DeletedTasks from '../views/DeletedTasks.vue'
+import DeletedView from '../views/DeletedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,15 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      meta: {
-        auth: true,
-      },
     },
     {
       path: '/deleted',
       name: 'deleted',
-      component: DeletedTasks,
-      props: true,
+      component: DeletedView,
     },
   ],
 })
