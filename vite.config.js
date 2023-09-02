c46257efdf2ca94a1482abc02c12cc0d9ed2ee4f
smola14 <dominik.smolinsky@icloud.com>
2023-09-02 19:42:26 +0200

    fix env

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': env,
-  },
   plugins: [vue()],
   resolve: {
     alias: {
